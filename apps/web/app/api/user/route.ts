d0a9a772721c3393bdea72bf4d6376cfe56123b2
anirbanpaulcom <anirbanpaulcom@gmail.com>
2024-06-28 03:48:02 +0530

    02

--- apps/web/app/api/user/route.ts
+++ apps/web/app/api/user/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import UserModel from '@/app/lib/database/model';
 import { connectToDatabase } from '@/app/lib/database/mongodb';
+
 export async function POST(req: NextRequest) {
   try {
     await connectToDatabase();
@@ -30,5 +31,35 @@ export async function POST(req: NextRequest) {
   } catch (error) {
     console.error('Error creating user:', error);
     return NextResponse.json({ error: 'Failed to create user' }, { status: 500 });
+  }
+}
+
+export async function GET(req: NextRequest, { params }: { params: { number: string } }) {
+  try {
+    await connectToDatabase();
+
+    // Ensure params and params.number are defined
+    const numberString = '10000000002'
+
+    if (!numberString) {
+      return NextResponse.json({ error: 'Missing number parameter' }, { status: 400 });
+    }
+
+    const number = parseInt(numberString, 10);
+
+    if (isNaN(number)) {
+      return NextResponse.json({ error: 'Invalid number format' }, { status: 400 });
+    }
+
+    const user = await UserModel.findOne({ number });
+
+    if (!user) {
+      return NextResponse.json({ error: 'User not found' }, { status: 404 });
+    }
+
+    return NextResponse.json(user);
+  } catch (error) {
+    console.error('Error fetching user details:', error);
+    return NextResponse.json({ error: 'Failed to fetch user details' }, { status: 500 });
   }
 }
